d077b7dd50154640dede1dce111631bb80b68d7f
Lucas Coutinho <lucxscout@gmail.com>
2023-05-31 17:13:26 -0300

    Exercícios em JS

---
+++ fundamentals/first-period/3.introducao-js/3.1/ex01.js
@@ -0,0 +1,10 @@
+let num1 = 20;
+let num2 = 2;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let mult = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+
+console.log(soma, subtracao, mult, divisao, modulo);
